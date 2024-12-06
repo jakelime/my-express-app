84d3b5fb626345a3ddcddeee4c81a6db5221c716
jakelime@TKSSPRO001 <jakelim070@gmail.com>
2024-12-06 14:31:59 +0800

    successful api calls with cors enabled

--- README.md
+++ README.md
@@ -8,9 +8,9 @@ npm install express mongoose
 
 ## .env file
 
-```text
+```shell
 DB_USERNAME=kama
 DB_PASSWORD=kamapw
 DB_NAME=my-database
-DB_URI=localhost:27017
+DB_URI="127.0.0.1:27017"
 ```
--- server.js
+++ server.js
@@ -1,28 +1,21 @@
+require("./db.js")
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
+const cors = require('cors');
 
-const DB_USERNAME = process.env.DB_USERNAME
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_NAME = process.env.DB_NAME
-const DB_URI = process.env.DB_URI
 const PORT = process.env.PORT || 5000;
-
 const app = express();
-const db_uri = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_URI}/${DB_NAME}`;
-console.log(db_uri);
-mongoose.connect(db_uri).then(() => {
-    console.log("MongoDB connected...");
-}).catch((err) => {
-    console.log(err);
-})
+const api_version = "/api/v1";
 
 app.use(express.json());
-
+app.use(cors({}))
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+    res.send("express app is running fine!");
 })
 
+app.get(`${api_version}/envis/get_username`, (req, res) => {
+    res.send({ message: "Arun Kumar!" });
+})
 
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
