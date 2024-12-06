583ad41402be3fd7de5033e3a6b2a5b51881762b
jakelime@TKSSPRO001 <jakelim070@gmail.com>
2024-12-06 13:03:46 +0800

    update env file. fix db connectionstr

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 ```shell
 npm install express mongoose
 ```
+
+## .env file
+
+```text
+DB_USERNAME=kama
+DB_PASSWORD=kamapw
+DB_NAME=my-database
+DB_URI=localhost:27017
+```
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express();
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
+const DB_URI = process.env.DB_URI
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/my-database', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+const app = express();
+const db_uri = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_URI}/${DB_NAME}`;
+console.log(db_uri);
+mongoose.connect(db_uri).then(() => {
+    console.log("MongoDB connected...");
+}).catch((err) => {
+    console.log(err);
+})
 
 app.use(express.json());
 
